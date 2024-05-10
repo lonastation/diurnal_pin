538cdc4e18c02c4a2bdc49c1cb46a6196546d337
iris_spp <iris.song@derbysoft.net>
2024-05-10 13:52:35 +0800

    girth filter query

--- app/src/main/java/com/linn/pin/data/girth/GirthsRepository.kt
+++ app/src/main/java/com/linn/pin/data/girth/GirthsRepository.kt
@@ -7,4 +7,5 @@ interface GirthsRepository {
     fun findAll(): Flow<List<Girth>>
     fun findNumber1AtAm(): Flow<List<Girth>>
     fun findNumber1AtPm(): Flow<List<Girth>>
+    fun findNumber2(): Flow<List<Girth>>
 }
